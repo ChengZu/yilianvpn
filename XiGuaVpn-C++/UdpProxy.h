5731036360e8fcef37565642d99435cfc6e880c3
ChengZu <1351606745@msn.cn>
2025-07-19 21:42:23 +0800

    Add files via upload

---
+++ XiGuaVpn-C++/UdpProxy.h
@@ -0,0 +1,216 @@
+#ifndef UDPPROXY_H
+#define UDPPROXY_H
+#include <ctime>
+#include "Loop.h"
+#include "Task.h"
+#include "IPHeader.h"
+#include "UDPHeader.h"
+#include "Socket.h"
+#include "DatagramSocket.h"
+#include "Config.h"
+
+class UdpProxy: public Task
+{
+public:
+	static const int HEADER_SIZE = IPHeader::IP4_HEADER_SIZE + UDPHeader::UDP_HEADER_SIZE;
+	
+	UdpProxy(){
+	}
+	
+	UdpProxy(Socket* socket, char* packet){
+		this->clientSocket = socket;
+		IPHeader ipHeader = IPHeader(packet, 0);
+		int ipHeaderLen = ipHeader.getHeaderLength();
+		UDPHeader udpHeader = UDPHeader(packet, ipHeaderLen);
+		srcIp = ipHeader.getSourceIP();
+		srcPort = udpHeader.getSourcePort();
+		destIp = ipHeader.getDestinationIP();
+		destPort = udpHeader.getDestinationPort();
+		
+		headerBytes = new char[UdpProxy::HEADER_SIZE];
+		CommonMethods::arraycopy(packet, 0, headerBytes, 0, IPHeader::IP4_HEADER_SIZE);
+		CommonMethods::arraycopy(packet, ipHeaderLen, headerBytes, IPHeader::IP4_HEADER_SIZE, UDPHeader::UDP_HEADER_SIZE);
+		ipHeader = IPHeader(headerBytes, 0);
+		udpHeader = UDPHeader(headerBytes, IPHeader::IP4_HEADER_SIZE);
+		ipHeader.setSourceIP(destIp);
+		udpHeader.setSourcePort(destPort);
+		ipHeader.setDestinationIP(srcIp);
+		udpHeader.setDestinationPort(srcPort);
+		ipHeader.setHeaderLength(20);
+		ipHeader.setProtocol(IPHeader::UDP);
+		ipHeader.setFlagsAndOffset(0);
+		closed = false;
+		lastRefreshTime = std::time(NULL);
+		
+		this->destSocket = DatagramSocket(destIp, destPort);
+		
+		iloop = Loop(this, (char*)"udpProxy");
+		iloop.join();
+		
+	}
+	
+	~UdpProxy(){
+		closed = true;
+		iloop.quit();
+		destSocket.iClose();
+		delete[] headerBytes;
+	}
+	
+	void close(){
+		closed = true;
+		iloop.quit();
+		destSocket.iClose();
+		// printf("[UdpProxy]代理(%ld)已关闭。\n", id);
+	}
+	
+	bool isClose(){
+		return closed;
+	}
+	
+	int sendToServer(char* bytes, int size, int offset = 0, int retry = 2){
+		lastRefreshTime = std::time(NULL);
+		
+		int len = size - offset;
+		char* data = new char[len];
+		CommonMethods::arraycopy(bytes, offset, data, 0, len);
+		int res = destSocket.socketSend(data, len);
+		delete[] data;
+		// 如果发送异常，放弃这个UDP连接 
+		if(res == 0){
+			close();
+		}else if(res == -1) {
+			perror("[UdpProxy]socket error msg");
+			printf("[UdpProxy]proxy(%ld%s) forward data to server fail, retry=%d.\n", id, toString().c_str(), retry);
+			if(retry > 0){
+				sendToServer(bytes, size, offset, retry - 1);
+			}
+		}else if((res > 0) && (res < size)){
+			sendToServer(bytes, size, (offset + res));
+		}
+		int dataSize = 0;
+		if(res > 0) dataSize = res;
+		if(retry == 0 && (offset + dataSize) != size){
+			close();
+			perror("[UdpProxy]socket error msg");
+			printf("[UdpProxy]proxy(%ld%s) forward data to server fail, total %d bytes, success send %d bytes.\n", id, toString().c_str(), size, (offset + dataSize));
+		}
+		return res;
+	}
+	
+	int sendToClient(char* bytes, int size, int offset = 0, int retry = 2){
+		lastRefreshTime = std::time(NULL);
+		
+		int len = size - offset;
+		char* data = new char[len];
+		CommonMethods::arraycopy(bytes, offset, data, 0, len);
+		int res = clientSocket->socketSend(data, len);
+		delete[] data;
+		// 如果发送异常，放弃这个UDP连接 
+		if(res == 0){
+			close();
+		}else if(res == -1) {
+			perror("[UdpProxy]socket error msg");
+			printf("[UdpProxy]proxy(%ld%s) forward data to client fail, retry=%d.\n", id, toString().c_str(), retry);
+			if(retry > 0){
+				sendToClient(bytes, size, offset, retry - 1);
+			}
+		}else if((res > 0) && (res < size)){
+			sendToClient(bytes, size, (offset + res));
+		}
+		int dataSize = 0;
+		if(res > 0) dataSize = res;
+		if(retry == 0 && (offset + dataSize) != size){
+			close();
+			perror("[UdpProxy]socket error msg");
+			printf("[UdpProxy]proxy(%ld%s) forward data to client fail, total %d bytes, success send %d bytes.\n", id, toString().c_str(), size, (offset + dataSize));
+		}
+		return res;
+	}
+	
+	void processPacket(std::vector<UdpProxy*> *udpProxys, char* packet, int size, bool newUdpProxy) {
+		if(newUdpProxy) {
+			udpProxys->push_back(this);
+		}
+		IPHeader ipHeader = IPHeader(packet, 0);
+		int ipHeaderLen = ipHeader.getHeaderLength();
+		UDPHeader udpHeader = UDPHeader(packet, ipHeaderLen);
+		int headerLen = ipHeaderLen + UDPHeader::UDP_HEADER_SIZE;
+		int dataSize = ipHeader.getTotalLength() - headerLen;
+		if(dataSize > 0){
+			char* data = new char[dataSize];
+			CommonMethods::arraycopy(packet, headerLen, data, 0, dataSize);
+			sendToServer(data, dataSize);
+			delete[] data;
+		}
+	}
+	
+	bool loop(){            
+		bool ret = false;
+		char* bytes = new char[Config::MUTE - UdpProxy::HEADER_SIZE];
+		int size = destSocket.socketRecv(bytes, Config::MUTE - UdpProxy::HEADER_SIZE);
+		if (size > 0) {
+			char* packet = new char[UdpProxy::HEADER_SIZE + size];
+			CommonMethods::arraycopy(headerBytes, 0, packet, 0, UdpProxy::HEADER_SIZE);
+			CommonMethods::arraycopy(bytes, 0, packet, UdpProxy::HEADER_SIZE, size);
+			updateUDPBuffer(packet, size);
+			sendToClient(packet, UdpProxy::HEADER_SIZE + size);
+			delete[] packet;
+		}else if(size == 0){
+			close();
+			ret = true;
+		}
+		delete[] bytes;
+		return ret;
+	}
+
+	void updateUDPBuffer(char* packet, int size){
+		IPHeader ipHeader = IPHeader(packet, 0);
+		UDPHeader udpHeader = UDPHeader(packet, IPHeader::IP4_HEADER_SIZE);
+		ipHeader.setTotalLength(UdpProxy::HEADER_SIZE + size);
+		udpHeader.setTotalLength(UDPHeader::UDP_HEADER_SIZE + size);
+		udpHeader.ComputeUDPChecksum(ipHeader);
+	}
+	
+	std::string toString(){
+		std::stringstream ss;
+    	ss <<"["<< CommonMethods::ipIntToString(destIp) << ":" << destPort << "]->[" << CommonMethods::ipIntToString(srcIp) << ":" << srcPort <<"]";
+    	return ss.str();
+	}
+	
+	bool isExpire(){
+		long now = std::time(NULL);
+		return (now - lastRefreshTime) > Config::UDPPROXY_EXPIRE_TIME;
+	}
+	
+	bool equal(char* packet) {
+		IPHeader ipHeader = IPHeader(packet, 0);
+		UDPHeader udpHeader = UDPHeader(packet, ipHeader.getHeaderLength());
+		return srcIp == ipHeader.getSourceIP() && srcPort == udpHeader.getSourcePort()
+				&& destIp == ipHeader.getDestinationIP()
+				&& destPort == udpHeader.getDestinationPort();
+	}
+	
+private:
+	// 客户端套接字 
+	Socket* clientSocket;
+	// 连接目标服务器的UDP套接字 
+	DatagramSocket destSocket;
+	// 保存UDP连接信息, 头部信息 
+	char* headerBytes;
+	// 已关闭状态 
+	bool closed;
+	// 源ip 
+	unsigned int srcIp;
+	// 源端口 
+	unsigned short srcPort;
+	// 目标ip 
+	unsigned int destIp;
+	// 目标端口 
+	unsigned short destPort; 
+	// 接收目标服务器数据任务 
+	Loop iloop;
+	// 最后刷新时间 
+	long lastRefreshTime;
+};
+
+#endif
---
+++ XiGuaVpn-C++/UdpTunnel.h
@@ -0,0 +1,122 @@
+#ifndef UDPTUNNEL_H
+#define UDPTUNNEL_H
+#include "UDPHeader.h"
+#include "UdpProxy.h"
+#include "Config.h"
+
+class UdpTunnel
+{
+public:
+	
+	UdpTunnel(){
+	}
+	
+	UdpTunnel(long id, Socket* socket){
+		this->id = id;
+		this->clientSocket = socket;
+		closed = false;
+	}
+	
+	~UdpTunnel(){
+		close();
+	}
+	
+	void close(){
+		closed = true;
+		clearAllProxy();
+	}
+	
+	bool isClose(){
+		return closed;
+	} 
+	
+	/*
+	 * 接收数据包 建立新代理
+	 */
+	void processPacket(char* packet, int size) {
+		// 清除已关闭代理，防止数据发送给已关闭代理  
+		clearCloseProxy();
+		// 清除长时间未活动代理，减小内存使用 
+		if (udpProxys.size() > Config::CLIENT_MAX_UDPPROXY) {
+			int clearNum = clearExpireProxy();
+			if (clearNum > 0) printf("[UdpTunnel]client(%ld) proxy number max, cleaned up number %d, now proxy number %ld.\n", id, clearNum, udpProxys.size());
+		}
+
+		// 检查该代理是否已创建 
+		UdpProxy* proxy = NULL;
+		for (int i = 0; i < udpProxys.size(); i++) {
+			if (udpProxys[i]->equal(packet)) {
+				proxy = udpProxys[i];
+				break;
+			}
+		}
+
+		// 代理没创建，建立新代理 
+		if (proxy == NULL) {
+			proxy = new UdpProxy(clientSocket, packet);
+			proxy->processPacket(&udpProxys, packet, size, true);
+		}else {
+			proxy->processPacket(&udpProxys, packet, size, false);
+		}
+	}
+	
+	/*
+	 * 清除过期代理
+	 */
+	int clearExpireProxy() {
+		int ret = 0;
+		for (int i = 0; i < udpProxys.size(); i++) {
+			UdpProxy* udpProxy = udpProxys[i];
+			if(udpProxy->isExpire()){
+				udpProxys.erase(udpProxys.begin() + i);
+				i--;
+				ret++;
+				delete udpProxy;
+			}
+		}
+		return ret;
+	}
+
+	/*
+	 * 清除已关闭代理
+	 */
+	int clearCloseProxy() {
+		int ret = 0;
+		for (int i = 0; i < udpProxys.size(); i++) {
+			UdpProxy* udpProxy = udpProxys[i];
+			if(udpProxy->isClose()){
+				udpProxys.erase(udpProxys.begin() + i);
+				i--;
+				ret++;
+				delete udpProxy;
+			}
+		}
+		return ret;
+	}
+
+	/*
+	 * 清除所有代理
+	 */
+	int clearAllProxy() {
+		int ret = udpProxys.size();
+		for (int i = 0; i < udpProxys.size(); i++) {
+			UdpProxy* udpProxy = udpProxys[0];
+			udpProxys.erase(udpProxys.begin());
+			i--;
+			delete udpProxy;
+		}
+		return ret;
+	}
+
+private:
+	// id 
+	long id;
+	// 已关闭标志 
+	bool closed;
+	// 客户端套接字  
+	Socket* clientSocket;
+	// 代理连接容器  
+	std::vector<UdpProxy*> udpProxys;
+};
+
+#endif
