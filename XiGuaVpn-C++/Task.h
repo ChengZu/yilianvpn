dde7d3ced25af05dbda1eb60eb13bafb4b6246bd
ChengZu <1351606745@msn.cn>
2025-07-23 22:09:16 +0800

    Add files via upload

---
+++ XiGuaVpn-C++/Task.h
@@ -0,0 +1,36 @@
+#ifndef TASK_H
+#define TASK_H
+
+class Task
+{
+public:
+	// 任务id 
+	long id;
+	//是否退出 
+	bool quit;
+	
+	Task(){
+		UID++;
+		id = UID;
+		quit = false;
+	}
+	
+	virtual ~Task(){
+		quit = true;
+	}
+	
+	// 虚构函数loop 
+	virtual bool loop(){
+		return false;
+	}
+	
+	bool operator==(const Task &task){
+		return id == task.id;
+	}
+private:
+	static long UID;
+};
+
+long Task::UID = 0;
+
+#endif
