04bc67da9954bd225dac356f4a0effc6db48c7f4
ChengZu <1351606745@msn.cn>
2026-02-12 09:46:22 +0800

    Add files via upload

---
+++ XiGuaVpn-C++/Task.h
@@ -0,0 +1,46 @@
+#ifndef TASK_H
+#define TASK_H
+
+class Task
+{
+public:
+	//是否退出
+	bool quit;
+
+	Task()
+	{
+		UID++;
+		id = UID;
+		quit = false;
+	}
+
+	virtual ~Task()
+	{
+		quit = true;
+	}
+
+	// 虚构函数loop
+	virtual bool loop()
+	{
+		return false;
+	}
+
+	bool operator==(const Task &task)
+	{
+		return id == task.id;
+	}
+
+	long getId()
+	{
+		return id;
+	}
+private:
+	// 任务id
+	long id;
+	static long UID;
+};
+
+long Task::UID = 0;
+
+#endif
+
---
+++ XiGuaVpn-C++/VpnServer.h
@@ -0,0 +1,83 @@
+#ifndef VPNSERVER_H
+#define VPNSERVER_H
+#include <vector>
+#include "Loop.h"
+#include "Client.h"
+#include "Socket.h"
+#include "ServerSocket.h"
+#include "Config.h"
+
+class VpnServer: public Task
+{
+public:
+	VpnServer(){
+		closed = false;
+		int res = server.init(0, Config::PORT);
+		if(res == -1){
+			perror("[VpnServer]socket error msg");
+			printf("[VpnServer]socket bind 0.0.0.0:%d fail.\n", Config::PORT);
+			close();
+		}else{
+			printf("[VpnServer]socket bind 0.0.0.0:%d success.\n", Config::PORT);
+			iloop = Loop(this, (char*)"VpnServer");
+			iloop.join();
+		}
+	}
+	
+	~VpnServer(){
+		close();
+	}
+	
+	void close(){
+		closed = true;
+		iloop.quit();
+		server.iClose();
+		for(int i=0; i<clients.size(); i++){
+			Client* client = clients[i];
+			delete client;
+		}
+	}
+	
+	bool isClose(){
+		return closed;
+	}
+	
+	bool loop(){
+		// 移除已关闭客户端 
+		for(int i=0; i<clients.size(); i++){
+			Client* client = clients[i];
+			if(client->isClosed() || client->isExpire()){
+				clients.erase(clients.begin() + i);
+				i--;
+				delete client;
+			}
+		}
+		int socket_fd = server.getClientSocket();
+		if(socket_fd <= 0){
+			return false;
+		}
+		Socket* socket = new Socket(socket_fd);
+		// 是否建立客户端 
+		if (clients.size() < Config::MAX_CLIENT_NUM) {
+			Client* client = new Client(socket);
+			clients.push_back(client);
+			printf("[VpnServer]new client(%ld)connecting, total client number %ld.\n", client->myId, clients.size());
+		} else {
+			socket->iClose();
+			printf("[VpnServer]client connet number reach max, total: %ld, client closeing.\n", clients.size());
+		}
+		return false;
+	}
+
+private:
+	// 服务器套接字 
+	ServerSocket server;
+	// 客户端容器 
+	std::vector<Client*> clients;
+	// 已关闭状态 
+	bool closed;
+	// 接收新客户端任务 
+	Loop iloop;
+};
+
+#endif
